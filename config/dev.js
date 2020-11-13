8753871e95923e39e763824d075b7c4db52b837a
igenex <igenexxx@gmail.com>
2020-11-13 17:19:36 +0300

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '930406961747-no02cvq553uoc5jgr4qkothvsikkv246.apps.googleusercontent.com',
-  googleClientSecret: 'GDdiHLKn0UIvHrha8eZce0gu',
-  mongoURI: 'mongodb://localhost:27017/blog_dev',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379',
-};
